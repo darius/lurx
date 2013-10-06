82fadb56b7fc699418d41ff034c2e9cf6cdeb568
Darius Bacon <darius@wry.me>
2013-10-06 11:44:40 +0200

    fix performance bug (tables aren't tuples)

--- lurx.lua
+++ lurx.lua
@@ -1,77 +1,85 @@
-local empty = {'literal', nil}
+local accepts = {}
 
-local function literal(ch)
-   return {'literal', string.byte(ch)}
-end
-
-local function either(r, s)
-   return {'either', r, s}
-end
-
-local function chain(r, s)
-   if r == empty then return s else return {'chain', r, s} end
-end
-
-local function star(r)
-   return {'star', r}
-end
-
-local nullable                  -- forward reference
-
--- Imagine all strings starting with ch that re matches; return a set
--- of regexes that among them match the remainders of those
--- strings. (For example, if ch is 'c', and re matches 'x', 'ca',
--- 'cat', and 'cow', and [q,r,s] are the keys in the result: that
--- means q|r|s must match 'a', 'at', and 'ow'.)  ch may be nil; in
--- this case return a nonempty set if re matches the empty string.
-local function after(ch, re)
-   local tag, r, s = re[1], re[2], re[3]
-   if tag == 'literal' then
-      local result = {}
-      if r == ch then result[empty] = true end
-      return result
-   elseif tag == 'either' then
-      local result = after(ch, r)
-      for k, _ in pairs(after(ch, s)) do
-         result[k] = true
-      end
-      return result
-   elseif tag == 'chain' then
-      local result = {}
-      for r_rest, _ in pairs(after(ch, r)) do
-         result[chain(r_rest, s)] = true
-      end
-      if nullable(r) then
-         for k, _ in pairs(after(ch, s)) do
-            result[k] = true
-         end
-      end
-      return result
-   elseif tag == 'star' then
-      local result = {}
-      if ch == nil then
-         result[empty] = true
-      else
-         for r_rest, _ in pairs(after(ch, r)) do
-            result[chain(r_rest, re)] = true
-         end
-      end
-      return result
+local function label(accepting, state)
+   if (accepting) then
+      accepts[state] = true
    else
-      for k, v in pairs(re) do print(k, v) end
-      assert(false)
+      assert(not accepts[state])
+   end
+   return state
+end
+
+local accept = label(true, function(ch, bound) return {} end)
+
+local function union(states1, states2)
+   local result = {}            -- XXX ok to clobber states1 instead?
+   for k, _ in pairs(states1) do
+      result[k] = true         
+   end
+   for k, _ in pairs(states2) do
+      result[k] = true         
+   end
+   return result
+end
+
+local function after(ch, start, bound)
+   if start == bound then       -- XXX is there an identity test instead?
+      return {}
+   else
+      return start(ch, bound)
    end
 end
 
--- Does r match the empty string?
-nullable = function(r)
-   return after(nil, r)[empty]
+local function lit(char, k)
+   local function act(ch, bound)
+      local result = {}
+      if char == ch then result[k] = true end
+      return result
+   end
+   return label(false, act)
 end
 
--- Is any of states nullable?
-local function any_nullable(states)
+local function alt(k1, k2)
+   local function act(ch, bound)
+      return union(after(ch, k1, bound), after(ch, k2, bound))
+   end
+   return label(accepts[k1] or accepts[k2], act)
+end
+
+local function star(r)
+   return function(k)
+      local body, loop
+      local function act(ch, bound)
+         return union(after(ch, body, loop),
+                      after(ch, k, bound))
+      end
+      loop = label(accepts[k], act)
+      body = r(loop)
+      return loop
+   end
+end
+
+local function empty(k)
+   return k
+end
+
+local function literal(ch)
+   local byte = string.byte(ch)
+   return function(k) return lit(byte, k) end
+end
+
+local function either(r, s)
+   return function(k) return alt(r(k), s(k)) end
+end
+
+local function chain(r, s)
+   return function(k) return r(s(k)) end
+end
+
+-- Is any of states accepting?
+local function any_accepts(states)
    for state, _ in pairs(states) do
-      if nullable(state) then return true end
+      if accepts[state] then return true end
    end
    return false
 end
@@ -80,28 +88,32 @@ end
 local function update(ch, states)
    local result = {}
    for state, _ in pairs(states) do
-      for k, _ in pairs(after(ch, state)) do
+      for k, _ in pairs(after(ch, state, accept)) do
          result[k] = true
       end
    end
    return result
 end
 
--- Return flag: does regular expression r match a prefix of string str? 
--- If so, return the length of the prefix as a second result.
-local function match(r, str)
-   if nullable(r) then
+local function run(start, str)
+   if accepts[start] then
       return true, 0
    end
    local states = {}
-   states[r] = true
+   states[start] = true
    for i = 1, #str do
       states = update(string.byte(str, i), states)
-      if any_nullable(states) then
+      if any_accepts(states) then
          return true, i
       end
    end
    return false
+end
+
+-- Return flag: does regular expression r match a prefix of string str? 
+-- If so, return the length of the prefix as a second result.
+local function match(re, str)
+   return run(re(accept), str)
 end
 
 return {
